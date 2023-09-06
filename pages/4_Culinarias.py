147451a286c916f3f1b787667e6f57442b7c5918
LEONARDO NASSIB FONSECA <141882326+leonardonassibfonseca@users.noreply.github.com>
2023-09-06 20:29:14 -0300

    Update 4_Culinarias.py

--- pages/4_Culinarias.py
+++ pages/4_Culinarias.py
@@ -613,10 +613,10 @@ with st.container():
     col1, col2 = st.columns(2)
 
     with col1:
-        st.markdown('##### Top 10 melhores tipos de culinárias')
+        st.markdown('##### Melhores tipos de culinárias')
         figura = tipo_culinaria_maior_nota_media(df)
         st.plotly_chart(figura, use_container_width = True)        
     with col2:
-        st.markdown('##### Top 10 piores tipos de culinárias')
+        st.markdown('##### Piores tipos de culinárias')
         figura = tipo_culinaria_menor_nota_media(df)
         st.plotly_chart(figura, use_container_width = True)  
